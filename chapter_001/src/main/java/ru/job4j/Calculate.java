fca435e68488ce28ef0984a4082dd2ece271c7cf
Deta4ed <deta4ed@gmail.com>
2018-05-07 21:10:30 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,24 @@
-package ru.job4j.calculate;
+/**
+* Package for calculate task.
+*
+* @author Lobach Vitaly (deta4ed@gmail.com) 
+* version $id$
+* since 0.1
+*/
+
+package ru.job4j;
+
+/**
+* Calculate
+*
+* @author Lobach Vitaly (deta4ed@gmail.com)
+*/
+
 public class Calculate {
+	/**
+	* Main.
+	* @param args - args.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
