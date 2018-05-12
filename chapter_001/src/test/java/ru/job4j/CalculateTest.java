31c2f5d3cb8766a224c36e1824ef1b038252c47c
Deta4ed <deta4ed@gmail.com>
2018-05-12 17:47:43 +0300

    formated

--- chapter_001/src/test/java/ru/job4j/CalculateTest.java
+++ chapter_001/src/test/java/ru/job4j/CalculateTest.java
@@ -7,23 +7,23 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
-* Test.
-*
-* @author Vitaly Lobach (deta4ed@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
+ * Test.
+ *
+ * @author Vitaly Lobach (deta4ed@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class CalculateTest {
 	/**
-	* Test echo.
-	*/ 
+	 * Test echo.
+	 */
 	@Test
 	public void whenTakeNameThenTreeEchoPlusName() {
-	    String input = "Vitaly";
-	    String expect = "Echo, echo, echo : Vitaly"; 
-	    Calculate calc = new Calculate();
-	    String result = calc.echo(input);
-	    assertThat(result, is(expect));
+		String input = "Vitaly";
+		String expect = "Echo, echo, echo : Vitaly";
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
 	}
- 
+
 }
