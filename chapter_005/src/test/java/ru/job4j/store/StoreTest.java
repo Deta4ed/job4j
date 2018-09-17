22d7324785abd3e7003aace0cfea3657af5def92
Deta4ed <deta4ed@gmail.com>
2018-09-17 06:25:04 +0300

    Статистику по коллекции.

--- chapter_005/src/test/java/ru/job4j/store/StoreTest.java
+++ chapter_005/src/test/java/ru/job4j/store/StoreTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.store;
 
-import org.junit.Before;
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,15 +8,28 @@ import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class StoreTest {
-    Store store = new Store();
-    List<Store.User> oldList = new ArrayList<>();
-    List<Store.User> newList = new ArrayList<>();
 
     @Test
     public void whenStoreTestThenChange() {
-        HashMap result = store.info(oldList, newList);
-        assertThat(result.get("Add"), is(0));
-        assertThat(result.get("Change"), is(0));
-        assertThat(result.get("Delete"), is(0));
+        List<Store.User> oldList = new ArrayList<>();
+        List<Store.User> newList = new ArrayList<>();
+        oldList.add(new Store.User(0, "Mike"));
+        oldList.add(new Store.User(1, "Petr"));
+        oldList.add(new Store.User(2, "Denil"));
+        oldList.add(new Store.User(3, "Bruce"));
+        oldList.add(new Store.User(4, "Dave"));
+        oldList.add(new Store.User(5, "Steve"));
+        oldList.add(new Store.User(6, "Max"));
+        newList.add(new Store.User(0, "Mike"));
+        newList.add(new Store.User(4, "Dave"));
+        newList.add(new Store.User(1, "Denil"));
+        newList.add(new Store.User(2, "Petr"));
+        newList.add(new Store.User(3, "Bruce"));
+        newList.add(new Store.User(7, "Max"));
+        newList.add(new Store.User(8, "Jon"));
+        HashMap result = new Store().info(oldList, newList);
+        assertThat(result.get("Add"), is(2));
+        assertThat(result.get("Change"), is(2));
+        assertThat(result.get("Delete"), is(2));
     }
 }
