f406e8cef92b569a13b9c821bc722f77c22d3ca0
Deta4ed <deta4ed@gmail.com>
2019-01-06 16:40:12 +0200

    4. В задании шахматы для поиска использовать Stream API.

--- chapter_002/src/main/java/ru/job4j/chess/Board.java
+++ chapter_002/src/main/java/ru/job4j/chess/Board.java
@@ -2,6 +2,8 @@ package ru.job4j.chess;
 
 import ru.job4j.chess.figures.Cell;
 import ru.job4j.chess.figures.Figure;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 /**
  * Board - chess board contains all the figures.
@@ -58,14 +60,7 @@ public class Board {
      * @throws OccupiedWayException
      */
     private boolean possibility(Cell[] steps) throws OccupiedWayException {
-        boolean potential = true;
-        for (Cell step : steps) {
-            for (Figure figure : figures) {
-                if (figure != null && step.equals(figure.position)) {
-                    potential = false;
-                }
-            }
-        }
+        boolean potential = Arrays.stream(steps).flatMap(s -> Arrays.stream(figures).filter(f -> f != null && s.equals(f.position))).count() == 0;
         if (!potential) {
             throw new OccupiedWayException("Occupied way exception!");
         }
@@ -79,13 +74,7 @@ public class Board {
      * @throws FigureNotFoundException
      */
     private int findBy(Cell cell) throws FigureNotFoundException {
-        int res = -1;
-        for (int index = 0; index != this.figures.length; index++) {
-            if (this.figures[index] != null && this.figures[index].position().equals(cell)) {
-                res = index;
-                break;
-            }
-        }
+        int res = IntStream.range(0, figures.length).filter(x -> figures[x] != null && figures[x].position().equals(cell)).findFirst().orElse(-1);
         if (res == -1) {
             throw new FigureNotFoundException("Figure not found exception!");
         }
